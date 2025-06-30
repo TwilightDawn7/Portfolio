99812a17db07445b86176e2ff55238467e2e7160
Manish <manishnath569@gmail.com>
2025-06-30 21:13:49 +0530

    Fixed some ui and added new projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,22 +1,8 @@
 import { RevealOnScroll } from "../RevealOnScroll";
+import projects from "@/lib/projectsList";
 
 export const Projects = () => {
-  const projects = [
-    {
-      name: "AI Coaching Voice Agent",
-      about:
-        "A real-time AI voice agent that transcribes and responds to user speech, simulating a personal coaching experience using Next.js, Convex, DeepSeek and Assembly AI.",
-      tech: ["TS", "NextJs", "React"],
-      url: "https://ai-coaching-voice-agent-sigma.vercel.app/",
-    },
-    {
-      name: "Weather App",
-      about:
-        "A sleek and responsive weather app that provides real-time weather updates for any city worldwide.",
-      tech: ["HTML", "CSS", "JS"],
-      url: "",
-    },
-  ];
+  
   return (
     <section
       id="projects"
---
+++ src/lib/projectsList.js
@@ -0,0 +1,27 @@
+
+
+const projects = [
+    {
+      name: "AI Coaching Voice Agent",
+      about:
+        "A real-time AI voice agent that transcribes and responds to user speech, simulating a personal coaching experience using Next.js, Convex, DeepSeek and Assembly AI.",
+      tech: ["TS", "NextJs", "React"],
+      url: "https://ai-coaching-voice-agent-sigma.vercel.app/",
+    },
+    {
+      name: "Streamify",
+      about:
+        "Streamify – A real-time video calling and messaging app designed for seamless communication with crystal-clear streams and instant chats.",
+      tech: ["React", "Vite", "JS"],
+      url: "https://streamify-av67.onrender.com",
+    },
+    {
+      name: "Weather App",
+      about:
+        "A sleek and responsive weather app that provides real-time weather updates for any city worldwide.",
+      tech: ["HTML", "CSS", "JS"],
+      url: "",
+    },
+  ];
+
+  export default projects;
